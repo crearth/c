4929ba5ed6eb4a320309e2f7516daeea098b3da7
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-03-07 17:51:26 +0530

    Create Sol.cpp

---
+++ Examples/Product Of Two Numbers/Sol.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+  cout<<"enter two integer numbers"<<endl;  
+  cin>>number1>>number2;
+
+
+    prod=number1*number2;
+  	 cout<<"Product of "<<" "<<number1<<" "<<"and"<<" "<<number2<<" "<<"is :" <<prod<<endl;
