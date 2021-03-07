4296501e40eb3ff7c3ac1435e1aebda50a6348a5
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-03-07 18:03:10 +0530

    Create Sol.cpp

---
+++ Examples/Incrementation and Decrementation/Sol.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a , b;
+cout<<"Enter two values a and b";
+cin>>a>>b;
+cout<<"a is :"<<" "<<a;
+cout<<"b is :"<<" "<<b;
+cout<<"a after incrementation :"<<" "<<a++;
+cout<<"b after incrementation :"<<" "<<b++;
+cout<<"a after decrementation :"<<" "<<a--;
+cout<<"b after decrementation :"<<" "<<b--;
+  
+return 0;
+}
