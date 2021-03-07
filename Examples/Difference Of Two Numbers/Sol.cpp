ba0bfe7444c0f1d951da6b796f5917527faa63e0
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-03-07 17:48:50 +0530

    Create Sol.cpp

---
+++ Examples/Difference Of Two Numbers/Sol.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+cout<<"enter two integer numbers"<<endl;  
+cin>>number1>>number2;
+
+ diff=(max(number1,number2))-(min(number1,number2));
+ cout<<"Difference of "<<" "<<(max(number1,number2))<<" "<<"and"<<" "<<(min(number1,number2))<<" "<<"is :" <<diff<<endl;
+
+  return 0;
+}
