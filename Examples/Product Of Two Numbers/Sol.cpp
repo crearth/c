3379b9b43fa09f59d202cd86f269c29f7a2019d0
Pandz18 <58665834+Pandz18@users.noreply.github.com>
2021-03-07 17:57:38 +0530

    Update Sol.cpp

--- Examples/Product Of Two Numbers/Sol.cpp
+++ Examples/Product Of Two Numbers/Sol.cpp
@@ -1,11 +1,16 @@
 #include<iostream>
 using namespace std;
 
-int main ()
+int main()
 {
-  cout<<"enter two integer numbers"<<endl;  
-  cin>>number1>>number2;
+int number1 , number2 , prod=1;
+cout<<"enter two integer numbers"<<endl;  
+cin>>number1>>number2;
 
 
-    prod=number1*number2;
-  	 cout<<"Product of "<<" "<<number1<<" "<<"and"<<" "<<number2<<" "<<"is :" <<prod<<endl;
+
+prod=number1*number2;
+cout<<"Product of "<<" "<<number1<<" "<<"and"<<" "<<number2<<" "<<"is :" <<prod<<endl;
+
+return 0;
+}
